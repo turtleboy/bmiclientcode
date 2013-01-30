721349383ac23f460a2effda882c901aac305f5d
jhodgski <james@mobilewebexpert.co.uk>
2013-01-30 23:07:53 +0000

    onMessage() String changd frm "price" to "update"
    onMessage() String changed from price to update, so it is now consistent
    with the PHP code

--- src/com/bmi/bmitestapp/GCMIntentService.java
+++ src/com/bmi/bmitestapp/GCMIntentService.java
@@ -6,20 +6,20 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
- 
+
 import com.google.android.gcm.GCMBaseIntentService;
- 
+
 import static com.bmi.bmitestapp.CommonUtilities.SENDER_ID;
 import static com.bmi.bmitestapp.CommonUtilities.displayMessage;
- 
+
 public class GCMIntentService extends GCMBaseIntentService {
- 
+
     private static final String TAG = "GCMIntentService";
- 
+
     public GCMIntentService() {
         super(SENDER_ID);
     }
- 
+
     /**
      * Method called on device registered
      **/
@@ -30,7 +30,7 @@ public class GCMIntentService extends GCMBaseIntentService {
         Log.d("NAME", MainActivity.name);
         ServerUtilities.register(context, MainActivity.name, MainActivity.email, registrationId);
     }
- 
+
     /**
      * Method called on device un registred
      * */
@@ -40,20 +40,20 @@ public class GCMIntentService extends GCMBaseIntentService {
         displayMessage(context, getString(R.string.gcm_unregistered));
         ServerUtilities.unregister(context, registrationId);
     }
- 
+
     /**
      * Method called on Receiving a new message
      * */
     @Override
     protected void onMessage(Context context, Intent intent) {
         Log.i(TAG, "Received message");
-        String message = intent.getExtras().getString("price");
- 
+        String message = intent.getExtras().getString("update");
+
         displayMessage(context, message);
         // notifies user
         generateNotification(context, message);
     }
- 
+
     /**
      * Method called on receiving a deleted message
      * */
@@ -65,7 +65,7 @@ public class GCMIntentService extends GCMBaseIntentService {
         // notifies user
         generateNotification(context, message);
     }
- 
+
     /**
      * Method called on Error
      * */
@@ -74,7 +74,7 @@ public class GCMIntentService extends GCMBaseIntentService {
         Log.i(TAG, "Received error: " + errorId);
         displayMessage(context, getString(R.string.gcm_error, errorId));
     }
- 
+
     @Override
     protected boolean onRecoverableError(Context context, String errorId) {
         // log message
@@ -83,7 +83,7 @@ public class GCMIntentService extends GCMBaseIntentService {
                 errorId));
         return super.onRecoverableError(context, errorId);
     }
- 
+
     /**
      * Issues a notification to inform the user that server has sent a message.
      */
@@ -93,9 +93,9 @@ public class GCMIntentService extends GCMBaseIntentService {
         NotificationManager notificationManager = (NotificationManager)
                 context.getSystemService(Context.NOTIFICATION_SERVICE);
         Notification notification = new Notification(icon, message, when);
- 
+
         String title = context.getString(R.string.app_name);
- 
+
         Intent notificationIntent = new Intent(context, MainActivity.class);
         // set intent so it does not start a new activity
         notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
@@ -104,14 +104,14 @@ public class GCMIntentService extends GCMBaseIntentService {
                 PendingIntent.getActivity(context, 0, notificationIntent, 0);
         notification.setLatestEventInfo(context, title, message, intent);
         notification.flags |= Notification.FLAG_AUTO_CANCEL;
- 
+
         // Play default notification sound
         notification.defaults |= Notification.DEFAULT_SOUND;
- 
+
         // Vibrate if vibrate is enabled
         notification.defaults |= Notification.DEFAULT_VIBRATE;
-        notificationManager.notify(0, notification);     
- 
+        notificationManager.notify(0, notification);
+
     }
- 
+
 }
